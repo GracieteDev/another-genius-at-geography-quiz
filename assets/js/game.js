ea9bd93fbd7610fe957c9f93ec8d36e9ad49b0bf
Graciete Gil <graciete.gil@gmail.com>
2024-04-24 22:49:16 +0100

    update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -60,10 +60,11 @@ startGame = () => {
 };
 
 getNewQuestion = () => {
+    // game.js
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
-        //go to the end page
-        return window.location.assign('/end.html');
+        // go to the end page
+         return window.location.assign('end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
